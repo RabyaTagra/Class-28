700d3121b0cd50ab91a47e8e582e3b5331dbf56f
RabyaTagra <rabyatagra35@gmail.com>
2020-10-05 18:00:16 +0530

    Project 28

--- Chain.js
+++ Chain.js
@@ -1,8 +1,8 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
@@ -11,10 +11,14 @@ class Chain{
     }
 
     display(){
+        if (this.chain.bodyA) {
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.chain.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
     }
-    
+    fly () {
+        this.chain.bodyA = null;
+    }
 }
